eb6d1ba9dc2f26da71830ee7ba30d064417924d9
TonYem <tonyrhesus@gmail.com>
2020-11-07 13:56:23 +0200

    refactor: improve basic js structure

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: {
-    main: path.resolve(__dirname, './src/index.js'),
+    main: path.resolve(__dirname, './src/js/index.js'),
   },
   output: {
     filename: '[name].[hash].js',
